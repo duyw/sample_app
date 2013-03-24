a63f215e4ff1f67cf69e23f38e3bd6b757e76ada
duyongwen <duyongwen@foxmail.com>
2013-03-24 18:33:54 +0800

    use test framework 'rspec'; add controller users

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,34 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
 
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
+    before { visit root_path }
 
-    it "should hava the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
   end
 
   describe "Help page" do
-    it "Should hava text 'Help'" do
-      visit "/static_pages/help"
-      page.should have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should hava the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About Page" do
-    it "Should hava content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should hava the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | About")
-    end
+    it { should have_selector('h1', text:'About Us') } 
+    it { should have_selector('title',text: full_title('About Us')) }
   end
 
+  describe "Contact Page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',text:'Contact') }
+    it { should have_selector('title',text:full_title('Contact')) }
+  end
 end
